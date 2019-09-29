ebf0d2083e835a736df9f1e93c5e59186ab66964
PangTiger <1346658359@qq.com>
2019-09-29 21:41:30 +0800

    Add 4 new files of STPU

--- src/Defines.v
+++ src/Defines.v
@@ -1,4 +1,4 @@
-// Global
+
 `define RstEnable 1'b1
 `define RstDisable 1'b0
 `define ZeroWord 32'h00000000
@@ -26,7 +26,6 @@
 `define ChipDisable 1'b0
 
 
-// Instructions
 `define EXE_AND  6'b100100
 `define EXE_OR   6'b100101
 `define EXE_XOR 6'b100110
@@ -42,8 +41,35 @@
 `define EXE_SRLV  6'b000110
 `define EXE_SRA  6'b000011
 `define EXE_SRAV  6'b000111
-`define EXE_SYNC  6'b001111
-`define EXE_PREF  6'b110011
+
+`define EXE_MOVZ  6'b001010
+`define EXE_MOVN  6'b001011
+`define EXE_MFHI  6'b010000
+`define EXE_MTHI  6'b010001
+`define EXE_MFLO  6'b010010
+`define EXE_MTLO  6'b010011
+
+`define EXE_SLT  6'b101010
+`define EXE_SLTU  6'b101011
+`define EXE_SLTI  6'b001010
+`define EXE_SLTIU  6'b001011   
+`define EXE_ADD  6'b100000
+`define EXE_ADDU  6'b100001
+`define EXE_SUB  6'b100010
+`define EXE_SUBU  6'b100011
+`define EXE_ADDI  6'b001000
+`define EXE_ADDIU  6'b001001
+`define EXE_CLZ  6'b100000
+`define EXE_CLO  6'b100001
+
+`define EXE_MULT  6'b011000
+`define EXE_MULTU  6'b011001
+`define EXE_MUL  6'b000010
+`define EXE_MADD  6'b000000
+`define EXE_MADDU  6'b000001
+`define EXE_MSUB  6'b000100
+`define EXE_MSUBU  6'b000101
+
 
 `define EXE_NOP 6'b000000
 `define SSNOP 32'b00000000000000000000000001000000
@@ -52,7 +78,7 @@
 `define EXE_REGIMM_INST 6'b000001
 `define EXE_SPECIAL2_INST 6'b011100
 
-// AluOp
+//AluOp
 `define EXE_AND_OP   8'b00100100
 `define EXE_OR_OP    8'b00100101
 `define EXE_XOR_OP  8'b00100110
@@ -69,24 +95,49 @@
 `define EXE_SRA_OP  8'b00000011
 `define EXE_SRAV_OP  8'b00000111
 
+`define EXE_MOVZ_OP  8'b00001010
+`define EXE_MOVN_OP  8'b00001011
+`define EXE_MFHI_OP  8'b00010000
+`define EXE_MTHI_OP  8'b00010001
+`define EXE_MFLO_OP  8'b00010010
+`define EXE_MTLO_OP  8'b00010011
+
+`define EXE_SLT_OP  8'b00101010
+`define EXE_SLTU_OP  8'b00101011
+`define EXE_SLTI_OP  8'b01010111
+`define EXE_SLTIU_OP  8'b01011000   
+`define EXE_ADD_OP  8'b00100000
+`define EXE_ADDU_OP  8'b00100001
+`define EXE_SUB_OP  8'b00100010
+`define EXE_SUBU_OP  8'b00100011
+`define EXE_ADDI_OP  8'b01010101
+`define EXE_ADDIU_OP  8'b01010110
+`define EXE_CLZ_OP  8'b10110000
+`define EXE_CLO_OP  8'b10110001
+
+`define EXE_MULT_OP  8'b00011000
+`define EXE_MULTU_OP  8'b00011001
+`define EXE_MUL_OP  8'b10101001
+`define EXE_MADD_OP  8'b10100110
+`define EXE_MADDU_OP  8'b10101000
+`define EXE_MSUB_OP  8'b10101010
+`define EXE_MSUBU_OP  8'b10101011
+
 `define EXE_NOP_OP    8'b00000000
 
-// AluSel
 `define EXE_RES_LOGIC 3'b001
 `define EXE_RES_SHIFT 3'b010
-//`define EXE_RES_ARITH 3'b100
+`define EXE_RES_MOVE 3'b011	
+`define EXE_RES_ARITHMETIC 3'b100	
+`define EXE_RES_MUL 3'b101
 
 `define EXE_RES_NOP 3'b000
 
-
-// Instruction ROM
 `define InstAddrBus 31:0
 `define InstBus 31:0
 `define InstMemNum 131071
 `define InstMemNumLog2 17
 
-
-// General Purpose Register
 `define RegAddrBus 4:0
 `define RegBus 31:0
 `define RegWidth 32
