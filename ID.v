036f29e55f0ecfbd8aa741f415a8efae09a9411c
BlackJackJam <37991184+BlackJackJam@users.noreply.github.com>
2019-09-29 16:15:20 +0800

    Assign modified

--- ID.v
+++ ID.v
@@ -68,107 +68,107 @@ always @(*) begin
         reg2_addr_o = `NOPRegAddr;
         imm_o = `ZeroWord;
     end else begin
-        aluop_o = `EXE_NOP_OP;
-        alusel_o = `EXE_RES_NOP;
-        wd_o = rd_addr;
-        reg1_read_o = `Disable;
-        reg2_read_o = `Disable;
-        reg1_addr_o = rs_addr;
-        reg2_addr_o = rt_addr;
-        imm_o = `ZeroWord;
+        aluop_o <= `EXE_NOP_OP;
+        alusel_o <= `EXE_RES_NOP;
+        wd_o <= rd_addr;
+        reg1_read_o <= `Disable;
+        reg2_read_o <= `Disable;
+        reg1_addr_o <= rs_addr;
+        reg2_addr_o <= rt_addr;
+        imm_o <= `ZeroWord;
         case (inst_op)
             `EXE_SPECIAL_INST:  begin
                 case (inst_func)
                     `EXE_OR: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_OR_OP;
-                        alusel_o = ``EXE_RES_LOGIC;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_OR_OP;
+                        alusel_o <= ``EXE_RES_LOGIC;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_AND: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_AND_OP;
-                        alusel_o = `EXE_RES_LOGIC;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_AND_OP;
+                        alusel_o <= `EXE_RES_LOGIC;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_XOR: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_XOR_OP;
-                        alusel_o = `EXE_RES_LOGIC;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_XOR_OP;
+                        alusel_o <= `EXE_RES_LOGIC;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_NOR: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_NOR_OP;
-                        alusel_o = `EXE_RES_LOGIC;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_NOR_OP;
+                        alusel_o <= `EXE_RES_LOGIC;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_SLLV: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_SLL_OP;
-                        alusel_o = `EXE_RES_SHIFT;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_SLL_OP;
+                        alusel_o <= `EXE_RES_SHIFT;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_SRLV: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_SRL_OP;
-                        alusel_o = `EXE_RES_SHIFT;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_SRL_OP;
+                        alusel_o <= `EXE_RES_SHIFT;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_SRAV: begin
-                         wreg_o = `WriteEnable;
-                        aluop_o = `EXE_SRA_OP;
-                        alusel_o = `EXE_RES_SHIFT;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                         wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_SRA_OP;
+                        alusel_o <= `EXE_RES_SHIFT;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_ADD: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_ADD_OP;
-                        alusel_o = `EXE_RES_ARITHMETIC;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_ADD_OP;
+                        alusel_o <= `EXE_RES_ARITHMETIC;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_SUB: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_SUB_OP;
-                        alusel_o = `EXE_RES_ARITHMETIC;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_SUB_OP;
+                        alusel_o <= `EXE_RES_ARITHMETIC;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_ADDU: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_ADDU_OP;
-                        alusel_o = `EXE_RES_ARITHMETIC;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_ADDU_OP;
+                        alusel_o <= `EXE_RES_ARITHMETIC;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_SUBU: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_SUBU_OP;
-                        alusel_o = `EXE_RES_ARITHMETIC;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_SUBU_OP;
+                        alusel_o <= `EXE_RES_ARITHMETIC;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_SLT: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_SLT_OP;
-                        alusel_o = `EXE_RES_ARITHMETIC;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_SLT_OP;
+                        alusel_o <= `EXE_RES_ARITHMETIC;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     `EXE_SLTU: begin
-                        wreg_o = `WriteEnable;
-                        aluop_o = `EXE_SLTU_OP;
-                        alusel_o = `EXE_RES_ARITHMETIC;
-                        reg1_read_o = `Enable;
-                        reg2_read_o = `Enable;
+                        wreg_o <= `WriteEnable;
+                        aluop_o <= `EXE_SLTU_OP;
+                        alusel_o <= `EXE_RES_ARITHMETIC;
+                        reg1_read_o <= `Enable;
+                        reg2_read_o <= `Enable;
                     end
                     default: begin
                     end
@@ -176,106 +176,106 @@ always @(*) begin
                 if (rs_addr == 5'h0) begin 
                     case (inst_func):
                         `EXE_SLL: begin
-                            wreg_o = `WriteEnable;
-                            aluop_o = `EXE_SLL_OP;
-                            alusel_o = `EXE_RES_SHIFT;
-                            reg1_read_o = `Disable;
-                            reg2_read_o = `Enable;
-                            imm_o = {27'h0,inst_sa};
-                            wd_o = rd_addr;
+                            wreg_o <= `WriteEnable;
+                            aluop_o <= `EXE_SLL_OP;
+                            alusel_o <= `EXE_RES_SHIFT;
+                            reg1_read_o <= `Disable;
+                            reg2_read_o <= `Enable;
+                            imm_o <= {27'h0,inst_sa};
+                            wd_o <= rd_addr;
                         end
                         `EXE_SRL: begin
-                            wreg_o = `WriteEnable;
-                            aluop_o = `EXE_SRL_OP;
-                            alusel_o = `EXE_RES_SHIFT;
-                            reg1_read_o = `Disable;
-                            reg2_read_o = `Enable;
-                            imm_o = {27'h0,inst_sa};
-                            wd_o = rd_addr;
+                            wreg_o <= `WriteEnable;
+                            aluop_o <= `EXE_SRL_OP;
+                            alusel_o <= `EXE_RES_SHIFT;
+                            reg1_read_o <= `Disable;
+                            reg2_read_o <= `Enable;
+                            imm_o <= {27'h0,inst_sa};
+                            wd_o <= rd_addr;
                         end
                         `EXE_SRA: begin
-                            wreg_o = `WriteEnable;
-                            aluop_o = `EXE_SRA_OP;
-                            alusel_o = `EXE_RES_SHIFT;
-                            reg1_read_o = `Disable;
-                            reg2_read_o = `Enable;
-                            imm_o = {27'h0,inst_sa};
-                            wd_o = rd_addr;
+                            wreg_o <= `WriteEnable;
+                            aluop_o <= `EXE_SRA_OP;
+                            alusel_o <= `EXE_RES_SHIFT;
+                            reg1_read_o <= `Disable;
+                            reg2_read_o <= `Enable;
+                            imm_o <= {27'h0,inst_sa};
+                            wd_o <= rd_addr;
                         end
                     endcase
                 end
             end
             `EXE_ORI: begin
-                wreg_o = `WriteEnable;
-                aluop_o = `EXE_OR_OP;
-                alusel_o = `EXE_RES_LOGIC;
-                reg1_read_o = `Enable;
-                reg2_read_o = `Disable;
-                imm_o = unsign_imm;
-                wd_o =  rt_addr;
+                wreg_o <= `WriteEnable;
+                aluop_o <= `EXE_OR_OP;
+                alusel_o <= `EXE_RES_LOGIC;
+                reg1_read_o <= `Enable;
+                reg2_read_o <= `Disable;
+                imm_o <= unsign_imm;
+                wd_o <=  rt_addr;
             end
             `EXE_ANDI: begin
-                wreg_o = `WriteEnable;
-                aluop_o = `EXE_AND_OP;
-                alusel_o = `EXE_RES_LOGIC;
-                reg1_read_o = `Enable;
-                reg2_read_o = `Disable;
-                imm_o = unsign_imm;
-                wd_o =  rt_addr;
+                wreg_o <= `WriteEnable;
+                aluop_o <= `EXE_AND_OP;
+                alusel_o <= `EXE_RES_LOGIC;
+                reg1_read_o <= `Enable;
+                reg2_read_o <= `Disable;
+                imm_o <= unsign_imm;
+                wd_o <=  rt_addr;
             end
             `EXE_XORI: begin
-                wreg_o = `WriteEnable;
-                aluop_o = `EXE_XOR_OP;
-                alusel_o = `EXE_RES_LOGIC;
-                reg1_read_o = `Enable;
-                reg2_read_o = `Disable;
-                imm_o = unsign_imm;
-                wd_o =  rt_addr;
+                wreg_o <= `WriteEnable;
+                aluop_o <= `EXE_XOR_OP;
+                alusel_o <= `EXE_RES_LOGIC;
+                reg1_read_o <= `Enable;
+                reg2_read_o <= `Disable;
+                imm_o <= unsign_imm;
+                wd_o <=  rt_addr;
             end
             `EXE_LUI: begin
-                wreg_o = `WriteEnable;
-                aluop_o = `EXE_OR_OP;
-                alusel_o = `EXE_RES_LOGIC;
-                reg1_read_o = `Disable;
-                reg2_read_o = `Disable;
-                imm_o = {imm,16'h0};
-                wd_o = rt_addr;
+                wreg_o <= `WriteEnable;
+                aluop_o <= `EXE_OR_OP;
+                alusel_o <= `EXE_RES_LOGIC;
+                reg1_read_o <= `Disable;
+                reg2_read_o <= `Disable;
+                imm_o <= {imm,16'h0};
+                wd_o <= rt_addr;
             end
             `EXE_ADDI: begin
-                wreg_o = `WriteEnable;
-                aluop_o = `EXE_ADD_OP;
-                alusel_o = `EXE_RES_ARITHMETIC;
-                reg1_read_o = `Enable;
-                reg2_read_o = `Disable;
-                imm_o = sign_imm;
-                wd_o = rt_addr;
+                wreg_o <= `WriteEnable;
+                aluop_o <= `EXE_ADD_OP;
+                alusel_o <= `EXE_RES_ARITHMETIC;
+                reg1_read_o <= `Enable;
+                reg2_read_o <= `Disable;
+                imm_o <= sign_imm;
+                wd_o <= rt_addr;
             end
             `EXE_ADDIU: begin
-                wreg_o = `WriteEnable;
-                aluop_o = `EXE_ADDU_OP;
-                alusel_o = `EXE_RES_ARITHMETIC;
-                reg1_read_o = `Enable;
-                reg2_read_o = `Disable;
-                imm_o = unsign_imm;
-                wd_o = rt_addr;
+                wreg_o <= `WriteEnable;
+                aluop_o <= `EXE_ADDU_OP;
+                alusel_o <= `EXE_RES_ARITHMETIC;
+                reg1_read_o <= `Enable;
+                reg2_read_o <= `Disable;
+                imm_o <= unsign_imm;
+                wd_o <= rt_addr;
             end
             `EXE_SLTI: begin
-                wreg_o = `WriteEnable;
-                aluop_o = `EXE_SLT_OP;
-                alusel_o = `EXE_RES_ARITHMETIC;
-                reg1_read_o = `Enable;
-                reg2_read_o = `Disable;
-                imm_o = sign_imm;
-                wd_o = rt_addr;
+                wreg_o <= `WriteEnable;
+                aluop_o <= `EXE_SLT_OP;
+                alusel_o <= `EXE_RES_ARITHMETIC;
+                reg1_read_o <= `Enable;
+                reg2_read_o <= `Disable;
+                imm_o <= sign_imm;
+                wd_o <= rt_addr;
             end
             `EXE_SLTIU: begin
-                wreg_o = `WriteEnable;
-                aluop_o = `EXE_SLTU_OP;
-                alusel_o = `EXE_RES_ARITHMETIC;
-                reg1_read_o = `Enable;
-                reg2_read_o = `Disable;
-                imm_o = unsign_imm;
-                wd_o = rt_addr;
+                wreg_o <= `WriteEnable;
+                aluop_o <= `EXE_SLTU_OP;
+                alusel_o <= `EXE_RES_ARITHMETIC;
+                reg1_read_o <= `Enable;
+                reg2_read_o <= `Disable;
+                imm_o <= unsign_imm;
+                wd_o <= rt_addr;
             end
             default: begin
             end
@@ -285,35 +285,35 @@ end
 
 always @(*) begin
     if (rst == `RstEnable) begin
-        reg1_o =  `ZeroWord;
+        reg1_o <=  `ZeroWord;
     end else if ((ex_wreg_i == `Enable) && (ex_wd_i == reg1_addr_o) && (reg1_read_o == `Enable)) begin
-        reg1_o = ex_wdata_i;
+        reg1_o <= ex_wdata_i;
     end else if ((reg1_read_o == `Enable) && (mem_wreg_i == `Enable) && (mem_wd_i == reg1_addr_o
     )) begin
-        reg1_o = mem_wdata_i;
+        reg1_o <= mem_wdata_i;
     end else if (reg1_read_o == `Enable) begin
-        reg1_o = reg1_data_i;
+        reg1_o <= reg1_data_i;
     end else if (reg1_read_o == `Disable) begin
-        reg1_o = imm_o;
+        reg1_o <= imm_o;
     end else begin
-        reg1_o = `ZeroWord;
+        reg1_o <= `ZeroWord;
     end
 end
 
 always @(*) begin
     if (rst == `RstEnable) begin
-        reg2_o =  `ZeroWord;
+        reg2_o <=  `ZeroWord;
     end else if ((ex_wreg_i == `Enable) && (ex_wd_i == reg1_addr_o) && (reg2_read_o == `Enable)) begin
-        reg2_o = ex_wdata_i;
+        reg2_o <= ex_wdata_i;
     end else if ((reg2_read_o == `Enable) && (mem_wreg_i == `Enable) && (mem_wd_i == reg2_addr_o
     )) begin
-        reg2_o = mem_wdata_i;
+        reg2_o <= mem_wdata_i;
     end else if (reg1_read_o == `Enable) begin
-        reg2_o = reg1_data_i;
+        reg2_o <= reg1_data_i;
     end else if (reg1_read_o == `Disable) begin
-        reg2_o = imm_o;
+        reg2_o <= imm_o;
     end else begin
-        reg2_o = `ZeroWord;
+        reg2_o <= `ZeroWord;
     end
 end
 
