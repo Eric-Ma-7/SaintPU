2f5f8f7c8d941af246657278590245f2c1c9b28a
PangTiger <1346658359@qq.com>
2019-09-29 21:33:13 +0800

    Update EX_MEM

--- src/EX_MEM.v
+++ src/EX_MEM.v
@@ -2,28 +2,28 @@
 
 module ex_mem (
 	//system signals
-	input	wire				rst, 
-	input	wire				clk,
+	input	wire			rst, 
+	input	wire			clk,
 	input   wire[`RegBus]		ex_wdata,
 	input   wire[`RegAddrBus]	ex_wd,
-	input   wire				ex_wreg,
+	input   wire			ex_wreg,
 
 	output  reg[`RegBus]		mem_wdata,
 	output  reg[`RegAddrBus]	mem_wd,
-	output	reg 				mem_wreg
+	output	reg 			mem_wreg
 	//
 );
     always @(posedge clk) begin
     	if (rst) begin
-    		// reset
-    		mem_wdata <= `ZeroWord;
-    		mem_wd <=  `NOPRegAddr;
-    		mem_wreg <= `WriteDisable;
+    	// reset
+    	    mem_wdata <= `ZeroWord;
+    	    mem_wd <=  `NOPRegAddr;
+    	    mem_wreg <= `WriteDisable;
     	end
     	else begin
-    		mem_wdata <= ex_wdata;
-    		mem_wd <= ex_wd;
-    		mem_wreg <= ex_wreg;
+    	    mem_wdata <= ex_wdata;
+    	    mem_wd <= ex_wd;
+    	    mem_wreg <= ex_wreg;
     	end
     end
 endmodule
