2686dfbb8d4b25dff4d6d537bb207b0af3038a9d
PangTiger <1346658359@qq.com>
2019-10-10 11:22:57 +0800

    Update EX_MEM to MADD MSUB version

--- src/EX_MEM.v
+++ src/EX_MEM.v
@@ -2,28 +2,48 @@
 
 module ex_mem (
 	//system signals
-	input	wire			rst, 
-	input	wire			clk,
+	input	wire				rst, 
+	input	wire				clk,
 	input   wire[`RegBus]		ex_wdata,
 	input   wire[`RegAddrBus]	ex_wd,
-	input   wire			ex_wreg,
-
+	input   wire				ex_wreg,
+    input   wire                ex_whilo,
+    input   wire[`RegBus]       ex_hi,
+    input   wire[`RegBus]       ex_lo,    
+    input   wire[5:0]           stall,
+    
+    output  reg                 mem_whilo,
+    output  reg[`RegBus]        mem_hi,
+    output  reg[`RegBus]        mem_lo,
 	output  reg[`RegBus]		mem_wdata,
 	output  reg[`RegAddrBus]	mem_wd,
-	output	reg 			mem_wreg
+	output	reg 				mem_wreg
 	//
 );
     always @(posedge clk) begin
-    	if (rst) begin
-    	// reset
+    	if (rst == `RstEnable) begin
+    		// reset
+    		mem_wdata <= `ZeroWord;
+    		mem_wd <= `NOPRegAddr;
+    		mem_wreg <= `WriteDisable;
+    		mem_whilo <= `WriteDisable;
+    		mem_hi <= `ZeroWord;
+    		mem_lo <= `ZeroWord;
+    	end else if(stall[3] == `Stop && stall[4] == `NoStop) begin
     	    mem_wdata <= `ZeroWord;
-    	    mem_wd <=  `NOPRegAddr;
-    	    mem_wreg <= `WriteDisable;
+    		mem_wd <= `NOPRegAddr;
+    		mem_wreg <= `WriteDisable;
+    		mem_whilo <= `WriteDisable;
+    		mem_hi <= `ZeroWord;
+    		mem_lo <= `ZeroWord;
+        end else if(stall[3] == `NoStop) begin
+    		mem_wdata <= ex_wdata;
+    		mem_wd <= ex_wd;
+    		mem_wreg <= ex_wreg;
+    		mem_whilo <= ex_whilo;
+    		mem_hi <= ex_hi;
+    		mem_lo <= ex_lo;
     	end
-    	else begin
-    	    mem_wdata <= ex_wdata;
-    	    mem_wd <= ex_wd;
-    	    mem_wreg <= ex_wreg;
-    	end
+    	//other: remain ex_mem regs' value
     end
 endmodule
